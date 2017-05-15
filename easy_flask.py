26a5af150900c6f11d686a1bd89d7a67e7c90f7b
Marty Heavey <martyheavey@Martys-MacBook-Pro-2.local>
2017-05-14 23:36:33 -0700

    removed app run()

--- easy_flask.py
+++ easy_flask.py
@@ -8,9 +8,6 @@ def index():
     return render_template("m4m_home.html")
 
 if __name__ == "__main__":
-    app.run()
-
-if __name__ == "__main__":
- app.debug = True
- port = int(os.environ.get('PORT', 5000))
- app.run(host="0.0.0.0", port=port)
+	app.debug = True
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host="0.0.0.0", port=port)
