dcf54021216d4c9d73901bfda5cfe82b060075e3
Marty Heavey <martyheavey@Martys-MacBook-Pro-2.local>
2017-12-05 12:08:38 -0500

    got titles for all originals

--- netscrape.py
+++ netscrape.py
@@ -7,6 +7,14 @@ uclient = urlopen(myurl)
 page_html = uclient.read()
 uclient.close()
 page_soup = soup(page_html, "html.parser")
-items = page_soup.findAll("div", {"class": "original-title-container"})
+items = page_soup.findAll("div", {"class": "original-title"})
+# item = items[0].text
+for item in items:
+	print item.text
+	
+# for item in items:
+# 	title = item.a.div["original-title"]
+# 	print title
+
 
 
