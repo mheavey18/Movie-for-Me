c14a5f17a8af43d34e986b4fa3f3b583bf481820
Marty Heavey <martyheavey@Martys-MacBook-Pro-2.local>
2017-12-05 15:21:57 -0500

    merging selenium and bs

--- net_login.py
+++ net_login.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup as soup
 
 browser = webdriver.Chrome('/Users/martyheavey/Downloads/chromedriver')
 browser.get('https://www.netflix.com/browse')
@@ -7,9 +8,18 @@ login_email = browser.find_element_by_css_selector('#email')
 login_pass = browser.find_element_by_css_selector('#password')
 submit = browser.find_element_by_css_selector('#appMountPoint > div > div.login-body > div > div > form:nth-child(2) > button')
 
-# login_email.send_keys('hiding username')
-# login_pass.send_keys('and password')
+login_email.send_keys('your email@cox.net')
+login_pass.send_keys('your pass')
 submit.click()
 
 marty_user = browser.find_element_by_css_selector('#appMountPoint > div > div > div.profiles-gate-container > div > div > ul > li:nth-child(3) > div > a > div > div')
 marty_user.click()
+
+html = browser.page_source
+
+page_soup = soup(html, "html.parser")
+
+# this is not working
+items = page_soup.findAll("div", {"class": "slider-item"})
+
+print items[0]
--- netscrape.py
+++ netscrape.py
@@ -1,36 +1,21 @@
 from bs4 import BeautifulSoup as soup
 from urllib2 import urlopen
-from selenium import webdriver
 
-browser = webdriver.Chrome('/Users/martyheavey/Downloads/chromedriver')
-browser.get('https://www.netflix.com/browse')
-
-login_email = browser.find_element_by_css_selector('#email')
-login_pass = browser.find_element_by_css_selector('#password')
-submit = browser.find_element_by_css_selector('#appMountPoint > div > div.login-body > div > div > form:nth-child(2) > button')
-
-# login_email.send_keys('chrisheavey@cox.net')
-# login_pass.send_keys('heavecat')
-# submit.click()
-
-marty_user = browser.find_element_by_css_selector('#appMountPoint > div > div > div.profiles-gate-container > div > div > ul > li:nth-child(3) > div > a > div > div')
-marty_user.click()
-
-# myurl = 'https://www.netflix.com/originals'
+myurl = 'https://www.netflix.com/originals'
 
 
-# uclient = urlopen(myurl)
-# page_html = uclient.read()
-# uclient.close()
-# page_soup = soup(page_html, "html.parser")
-# items = page_soup.findAll("div", {"class": "original-title"})
-# # item = items[0].text
-# for item in items:
-# 	print item.text
+uclient = urlopen(myurl)
+page_html = uclient.read()
+uclient.close()
+page_soup = soup(page_html, "html.parser")
+items = page_soup.findAll("div", {"class": "original-title"})
+# item = items[0].text
+for item in items:
+	print item.text
 	
-# for item in items:
-# 	title = item.a.div["original-title"]
-# 	print title
+for item in items:
+	title = item.a.div["original-title"]
+	print title
 
 
 
